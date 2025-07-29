da07bcdfe93978799ea8b584a6758d6f32a12bf4
Fabricio Dias <fabricio270909@gmail.com>
2025-07-28 22:53:02 -0300

    feat: Comparing and sorting Product by name

---
+++ ExpLambdaDelegatesLINQ/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿namespace ExpLambdaDelegatesLINQ.Entities;
+
+public class Product : IComparable<Product> {
+    public string Name { get; set; }
+    public double Price { get; set; }
+    
+    public Product(string name, double price) {
+        Name = name;
+        Price = price;
+    }
+    
+    public override string ToString() {
+        return $"{Name} - {Price:C}";
+    }
+    
+    // Implementing IComparable to allow sorting by Name
+    // This will sort the products by their names in a case-insensitive manner
+    public int CompareTo(Product? other) {
+        return Name.ToUpper().CompareTo(other.Name.ToUpper());
+    }
+}
--- ExpLambdaDelegatesLINQ/Program.cs
+++ ExpLambdaDelegatesLINQ/Program.cs
@@ -1,7 +1,40 @@
-﻿namespace ExpLambdaDelegatesLINQ;
+﻿using ExpLambdaDelegatesLINQ.Entities;
+
+namespace ExpLambdaDelegatesLINQ;
 
 class Program {
     static void Main(string[] args) {
-        Console.WriteLine("Hello, World!");
+        int option;
+        do {
+            Console.Clear();
+            Console.WriteLine("1. Use Comparison with products");
+            Console.WriteLine("6. Exit");
+            Console.Write("Choose an option: ");
+            option = int.Parse(Console.ReadLine() ?? "0");
+            switch (option) {
+                case 1:
+                    List<Product> productList = new List<Product> {
+                        new Product("Laptop", 1200.00),
+                        new Product("Smartphone", 800.00),
+                        new Product("Tablet", 300.00)
+                    };
+                    
+                    // Sort based on the CompareTo method
+                    productList.Sort();
+
+                    foreach (var product in productList) {
+                        Console.WriteLine(product);
+                    }
+                    break;
+                case 6:
+                    Console.WriteLine("Exiting...");
+                    break;
+                default:
+                    Console.WriteLine("Invalid option. Please try again.");
+                    break;
+            }
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        } while (option != 6);
     }
 }
